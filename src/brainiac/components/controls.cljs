830b7d61901c30cbe88d8d791c4e64f776e41b2d
Anton Verinov <git@zemlan.in>
2015-10-26 18:47:15 +0200

    import/export app-state

--- src/brainiac/components/controls.cljs
+++ src/brainiac/components/controls.cljs
@@ -1,10 +1,18 @@
 (ns ^:figwheel-always brainiac.components.controls
     (:require [rum.core :as rum]
               [brainiac.appstate :as app]
-              [cljs.core.async :refer [>! <! put! chan]]
-              [cljs.core.match :refer-macros [match]]))
+              [cljs.reader :refer [read-string]]))
+
+(defn export-state []
+  (js/prompt "Copy current state" (dissoc @app/app-state :search-result)))
+
+(defn import-state []
+  (let [new-state (read-string (js/prompt "Paste state"))]
+    (reset! app/app-state new-state)))
 
 (rum/defc controls-component []
   [:div
-    [:a {:className "action fa-cloud-download"}]
-    [:a {:className "action fa-cloud-upload"}]])
+    [:a {:className "action fa-cloud-download"
+          :onClick export-state}]
+    [:a {:className "action fa-cloud-upload"
+          :onClick import-state}]])
