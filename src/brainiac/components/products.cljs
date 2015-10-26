06f5d8fd3867dc31fffab1aeb308793a261fa08f
Anton Verinov <git@zemlan.in>
2015-10-26 18:09:35 +0200

    prettier products

--- src/brainiac/components/products.cljs
+++ src/brainiac/components/products.cljs
@@ -2,12 +2,14 @@
     (:require [rum.core :as rum]
               [brainiac.appstate :as app]))
 
-(defn product-component [data]
-  [:div {:key (:_id data)} (:_id data)])
+(defn product-component [{_id :_id {n :name} :_source :as data}]
+  [:div {:key _id
+          :className "pure-u-1-3"}
+    _id " / " n])
 
 (rum/defc products-component < rum/reactive []
   (let [search-result (:search-result (rum/react app/app-state))
         {{total :total products :hits :as hits} :hits} search-result]
-    [:div
-      [:h3 (if total (str "products / " total) "products")]
+    [:div {:className "pure-g"}
+      [:h3 {:className "pure-u-1"} (if total (str "products / " total) "products")]
       [:div (map product-component products)]]))
